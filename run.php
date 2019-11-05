970e62b49b8f9229d754e9910e3297dc5293c710
KhanShahzeen <shahzeen.razakhan2@aiesec.net>
2019-11-05 17:51:03 +0500

    Cloned, Updated with new branch named : (branch_Shahzeen)

--- run.php
+++ run.php
@@ -1,24 +1,64 @@
+include 'checking.html'
 <?php
+
+use Mpdf\Mpdf;
+use Smalot\PdfParser\Parser;
+
+require_once __DIR__ . '/vendor/autoload.php';
 
 include 'vendor/autoload.php';
 include 'lib/HelperFunctions.php';
 
-$parser = new \Smalot\PdfParser\Parser();
-$pdf = $parser->parseFile('input/10.pdf');
+//parsing html to pdf
+$mpdf = new Mpdf();
+
+//parsing pdf into text
+$parser = new Parser();
+$pdf = $parser->parseFile('input/1.pdf');
 
 $text = $pdf->getText();
 
 $pieces = explode("\n", $text);
 $candidateName = trim($pieces[0]);
 
-echo '<br>==Candidate==<br>';
-echo $candidateName;
+$summary = convertNewlineIntoLineBreak(getStringBetween($text, 'Summary', 'Experience'));
 
-echo '<br>==Summary==<br>';
-echo convertNewlineIntoLineBreak(getStringBetween($text, 'Summary', 'Experience'));
+$experience = convertNewlineIntoLineBreak(getStringBetween($text, 'Experience', 'Education'));
 
-echo '<br>==Experience==<br>';
-echo convertNewlineIntoLineBreak(getStringBetween($text, 'Experience', 'Education'));
+$education = convertNewlineIntoLineBreak(getStringBetween($text, 'Education', $candidateName));;
 
-echo '<br>==Education==<br>';
-echo convertNewlineIntoLineBreak(getStringBetween($text, 'Education', $candidateName));
+$html = '';
+
+
+$html = '';
+
+$html .= '<div class="jumbotron">';
+$html .= '    <div class="mainHeading" style="color:white;
+                width: 100%;
+                text-align:center;
+                background-color:black;">';
+$html .= '           <h1>' . $candidateName .'</h1>';
+$html .= '    </div>';
+$html .= '</div>';
+$html .= '<br/> <br/> <br/> <br/>';
+$html .= '<h3>SUMMARY</h3>';
+$html .= '<p>' . $summary . '</p>';
+$html .= '<br/> <br/> <br/> <br/>';
+$html .= '<h3>EXPERIENCE</h3>';
+$html .= '<p>' . $experience . '</p>';
+$html .= '<br/> <br/> <br/> <br/>';
+$html .= '<h3>EDUCATION</h3>';
+$html .= '<p>' . $education . '</p>';
+
+$mpdf->WriteHTML($html);
+$mpdf->Output();
+
+
+
+
+
+
+
+
+
+
