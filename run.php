33ea1495699f8e3dd5ddbd005db700f78ba156fd
KhanShahzeen <shahzeen.razakhan2@aiesec.net>
2019-11-06 13:10:55 +0500

    csv reading

--- run.php
+++ run.php
@@ -1,7 +1,7 @@
-include 'checking.html'
 <?php
-
+use Mpdf\HTMLParserMode;
 use Mpdf\Mpdf;
+use Mpdf\Output\Destination;
 use Smalot\PdfParser\Parser;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -9,56 +9,62 @@ require_once __DIR__ . '/vendor/autoload.php';
 include 'vendor/autoload.php';
 include 'lib/HelperFunctions.php';
 
-//parsing html to pdf
-$mpdf = new Mpdf();
 
-//parsing pdf into text
-$parser = new Parser();
-$pdf = $parser->parseFile('input/1.pdf');
+$the_big_array = [];
 
-$text = $pdf->getText();
+// Open the file for reading
 
-$pieces = explode("\n", $text);
-$candidateName = trim($pieces[0]);
+if (($h = fopen("lib/Cosmo Shahzeen.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($h, 1000, ",")) !== FALSE) {
+        $the_big_array[] = $data;
+    }
 
-$summary = convertNewlineIntoLineBreak(getStringBetween($text, 'Summary', 'Experience'));
+    fclose($h);
+}
 
-$experience = convertNewlineIntoLineBreak(getStringBetween($text, 'Experience', 'Education'));
+//$stylesheet = file_get_contents('lib/main.css');
 
-$education = convertNewlineIntoLineBreak(getStringBetween($text, 'Education', $candidateName));;
+for($i = 1; $i<sizeof($the_big_array);$i++) {
 
-$html = '';
+    //parsing html to pdf
+    $mpdf = new Mpdf();
 
+    //parsing pdf into text
+    $parser = new Parser();
 
-$html = '';
+    $pdf = $parser->parseFile('input/' . $the_big_array[$i][0] . '.pdf');
 
-$html .= '<div class="jumbotron">';
-$html .= '    <div class="mainHeading" style="color:white;
-                width: 100%;
-                text-align:center;
-                background-color:black;">';
-$html .= '           <h1>' . $candidateName .'</h1>';
-$html .= '    </div>';
-$html .= '</div>';
-$html .= '<br/> <br/> <br/> <br/>';
-$html .= '<h3>SUMMARY</h3>';
-$html .= '<p>' . $summary . '</p>';
-$html .= '<br/> <br/> <br/> <br/>';
-$html .= '<h3>EXPERIENCE</h3>';
-$html .= '<p>' . $experience . '</p>';
-$html .= '<br/> <br/> <br/> <br/>';
-$html .= '<h3>EDUCATION</h3>';
-$html .= '<p>' . $education . '</p>';
+    $text = $pdf->getText();
 
-$mpdf->WriteHTML($html);
-$mpdf->Output();
+    $pieces = explode("\n", $text);
+    $candidateName = trim($pieces[0]);
 
+    $summary = convertNewlineIntoLineBreak(getStringBetween($text, 'Summary', 'Experience'));
 
+    $experience = convertNewlineIntoLineBreak(getStringBetween($text, 'Experience', 'Education'));
 
+    $education = convertNewlineIntoLineBreak(getStringBetween($text, 'Education', $candidateName));;
 
+    $html = '';
 
+    $html .= '<div class="container">';
+    $html .= '    <div class="mainHeading">';
+    $html .= '           <h1>' . $candidateName . '</h1>';
+    $html .= '    </div>';
+    $html .= '</div>';
+    $html .= '<br/> <br/> <br/> <br/>';
+    $html .= '<h3>SUMMARY</h3>';
+    $html .= '<p>' . $summary . '</p>';
+    $html .= '<br/> <br/> <br/> <br/>';
+    $html .= '<h3>EXPERIENCE</h3>';
+    $html .= '<p>' . $experience . '</p>';
+    $html .= '<br/> <br/> <br/> <br/>';
+    $html .= '<h3>EDUCATION</h3>';
+    $html .= '<p>' . $education . '</p>';
 
-
-
+    //$mpdf->WriteHTML($stylesheet, HTMLParserMode::HEADER_CSS);
+    $mpdf->WriteHTML($html, HTMLParserMode::HTML_BODY);
+    $mpdf->Output("output" . $the_big_array[$i][0] . ".pdf", "F");
+}
 
 
