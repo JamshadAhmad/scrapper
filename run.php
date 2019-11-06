4f9b739ede96312288933a03f0e1452192b65b6b
KhanShahzeen <shahzeen.razakhan2@aiesec.net>
2019-11-06 18:53:03 +0500

    Font resolved issue

--- run.php
+++ run.php
@@ -33,13 +33,14 @@ $mpdfConfig = array(
     'mode' => 'utf-8',
     'format' => 'A4',    // format - A4, for example, default ''
     'default_font_size' => 0,     // font size - default 0
-    'default_font' => '',    // default font family
+    'default_font' => 'serif',    // default font family
     'margin_left' => 0,    	// 15 margin_left
     'margin_right' => 0,    	// 15 margin right
     'margin_top' => 0,
+    'margin_bottom' => 0,
     'margin_header' => 0,     // 9 margin header
     'margin_footer' => 0,     // 9 margin footer
-    'orientation' => 'P'  	// L - landscape, P - portrait
+    'orientation' => 'P',  	// L - landscape, P - portrait
 );
 
     //parsing html to pdf
@@ -55,7 +56,7 @@ $mpdfConfig = array(
     $pieces = explode("\n", $text);
     $candidateName = trim($pieces[0]);
 
-    $summary = convertNewlineIntoLineBreak(getStringBetween($text, 'Summary', 'Experience'));
+    $summary = getStringBetween($text, 'Summary', 'Experience');
 
     $experience = convertNewlineIntoLineBreak(getStringBetween($text, 'Experience', 'Education'));
 
@@ -63,32 +64,50 @@ $mpdfConfig = array(
 
     $html = '';
 
-    $html .= '<div class="cvName container-fluid text-center">';
-    $html .= '    <div class="mainHeading">';
-    $html .= '           <h1>' . $candidateName . '</h1>';
-    $html .= '    </div>';
-    $html .= '</div>';
-    $html .= '<div class="container-fluid">';
-    $html .= '    <div class="row">';
-    $html .= '        <div class="col-xs-8 ">';
-    $html .= '            <div class="infoLeftSection">';
-    $html .= '                <div class="mainDetails">';
-    $html .= '                    <div class="Objective">Objectives:' . $summary . '</div>';
-    $html .= '                    <div class="Experience">Experience:' . $experience . '</div>';
-    $html .= '                </div>';
-    $html .= '            </div>';
-    $html .= '        </div>';
-    $html .= '        <div class="col-xs-3 ">';
-    $html .= '            <div class="infoRightSection">';
-    $html .= '                <div class="extraDetails">';
-    $html .= '                    <div class="email">Email:' . $the_big_array[1][2] . ' </div>';
-    $html .= '                    <div class="phone">Phone:' . $the_big_array[1][3] . '</div>';
-    $html .= '                    <div class="linkL">Link:' . $the_big_array[1][1] . '</div>';
-    $html .= '                </div>';
-    $html .= '            </div>';
-    $html .= '        </div>';
-    $html .= '    </div>';
-    $html .= '</div>';
+    $html .= '
+    <body>           
+        <div class="cvName container-fluid text-center">
+            <div class="mainHeading">
+                <h1 style="font-weight: bold;">' . $candidateName . '</h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-8 ">
+                    <div class="infoLeftSection">
+                        <div class="mainDetails">
+                            <div class="Objective">
+                                <p style="font-weight: bold">Objectives:</p>
+                                <p style="font-family: Arial">' . $summary . '</p>
+                            </div>
+                            <div class="Experience">
+                                <p id="PE" style="font-weight: bold;">Experience:</p>
+                                <p>' . $experience . '</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-3 ">
+                    <div class="infoRightSection">
+                        <div class="extraDetails">
+                            <div class="email">
+                                <p style="font-weight: bold">Email:</p>
+                                <p>' . $the_big_array[1][2] . '</p>
+                            </div>
+                            <div class="phone">
+                                <p style="font-weight: bold">Phone:</p>
+                                <p>' . $the_big_array[1][3] . '</p>
+                            </div>
+                            <div class="linkL">
+                                <p style="font-weight: bold">Link:</p>
+                                ' . $the_big_array[1][1] . '
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>';
 
 //    $html .= '<br/> <br/> <br/> <br/>';
 //    $html .= '<h3>SUMMARY</h3>';
@@ -99,20 +118,6 @@ $mpdfConfig = array(
 //    $html .= '<br/> <br/> <br/> <br/>';
 //    $html .= '<h3>EDUCATION</h3>';
 //    $html .= '<p>' . $education . '</p>';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     $mpdf->WriteHTML($stylesheet, HTMLParserMode::HEADER_CSS);
