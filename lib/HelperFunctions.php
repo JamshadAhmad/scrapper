8e5724ff684e65cca67597ceaed890516eca9c37
KhanShahzeen <shahzeen.razakhan2@aiesec.net>
2019-11-14 20:05:18 +0500

    14Nov19

--- lib/HelperFunctions.php
+++ lib/HelperFunctions.php
@@ -1,62 +1,19 @@
 <?php
 
 /**
- * Returns string between two substrings
- * @param $string
- * @param $start
- * @param $end
- * @return bool|string
- */
-function getStringBetween($string, $start = '', $end = '')
-{
-    if ($start != '') {
-        $string = ' ' . $string;
-        $ini = strpos($string, $start);
-        if ($ini == 0) {
-            return '';
-        }
-        $ini += strlen($start);
-    } else {
-        $ini = 0;
-    }
-
-    if ($end == '') {
-        return substr($string, $ini);
-    }
-    $len = strpos($string, $end, $ini) - $ini;
-    return substr($string, $ini, $len);
-}
-
-/**
- * Converts \n into <br> tag
- * @param $input
- * @return mixed
- */
-function convertNewlineIntoLineBreak($input)
-{
-    return str_replace("\n", '<br>', $input);
-}
-
-/**
  * Parsed Meaningfull Text
  * @param $text
  * @return mixed
  */
 function convertPropertext($text)
 {
-    $sum = getStringBetween($text, 'Summary', 'Experience');
-    $ch = explode("\n", $sum);
-
     $string = '';
-    $len = count($ch);
-    for ($i = 0; $i < $len; $i++) {
-//        $ch[$i].="\n";
-        if (strlen($ch[$i]) < 68) {
-//            $ch[$i].="\n";
-            $string .= $ch[$i] . "\n";
+    $len = count($text);
+    for ($i = 0; $i < $len; ++$i) {
+        if (strlen($text[$i]) <= 70) {
+            $string .= $text[$i] . '<br>';
         } else {
-//            $ch[$i] = str_replace("\n", "-", $ch[$i]);
-            $string .= $ch[$i];
+            $string .= ' ' . $text[$i];
         }
     }
     return str_replace("\n", "<br>", $string);
@@ -88,3 +45,11 @@ function whichHeading($fontsize) {
     else return 'text';
 }
 
+/**
+ * @param $text
+ * @return string
+ */
+function bulletCheck ($text)
+{
+   return str_replace('•',"<br> •", $text);
+}
--- run.php
+++ run.php
@@ -34,6 +34,22 @@ do{
 
 $document = new Document($finalHtml);
 $spans = $document->find('span');
+
+
+
+//foreach($spans as $span) {
+//     if (trim(getFontSize($span -> style), "px") == 20)
+//     {
+//         $title .= $span -> text();
+//     }
+//     elseif (trim(getFontSize($span -> style), "px") == 16 && ($span -> text()) == "Summary")
+//     {
+//         $summary
+//     }
+//    echo ("   " . $span -> text());
+//    echo ("\n");
+//}
+
 $i = -1;
 
 foreach($spans as $span) {
@@ -46,52 +62,104 @@ foreach($spans as $span) {
         $sections[$i][$heading][] = $span -> text();
     }
 }
+//print_r($sections);
+//die;
+
+$summary = [];
+$experience = [];
+$education = [];
+$title = '';
+
+$sec_len = count($sections);
+//print_r($sections[3]['bigheading']);
+//print_r($sections[4]['bigheading']);
+//print_r($sections[5]['bigheading']);
+//die;
+
+for($x = 3;$x < 6;$x++)
+{
+    if($sections[$x]['bigheading'] == "Summary")
+    {
+        $summary = $sections[$x]['text'];
+    }
+    else if($sections[$x]['bigheading'] == "Experience")
+    {
+        $experience = $sections[$x]['text'];
+    }
+    else if($sections[$x]['bigheading'] == "Education")
+    {
+        $education = $sections[$x]['text'];
+    }
+}
+
+//summary
+$ex = convertPropertext($summary);
+$final_summary = bulletCheck($ex);
+$final_summary = str_replace("<br> <br>", "<br>", $final_summary);
+
+//experience
+$ex2 = convertPropertext($experience);
+
+
 $html = '';
 $html .= '
-        
-<body>
-    <div class="cvName container-fluid text-center">
-        <div class="mainHeading">
-            <h1 id="yay">' . $sections[0]['hugeheading'] . '</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width"/>
+        <link type="text/css" rel="stylesheet" href="lib/main.css">
+        <link type="text/css" rel="stylesheet" href="lib/bootstrap.min.css">
+    </head>        
+    <body>
+        <div class="cvName container-fluid text-center">
+            <div class="mainHeading">
+                <h1 id="yay">' . $sections[0]['hugeheading'] . ' </h1>
+                <h4 id="yay">' . $sections[1]['subheading'] . ' </h4>
+                <h4 id="yay">' . $sections[2]['subheading'] . ' </h4>
+            </div>
         </div>
-    </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-8 ">
-                <div class="infoLeftSection">
-                    <div class="mainDetails">
-                        <div class="Objective">
-                            <p style="font-weight: bold">OBJECTIVES:</p>
-                            <p></p>
+        <div class="container-fluid">
+            <div class="row">   
+                <div class="col-xs-8" style="width: 69.99%;!important;">
+                    <div class="infoLeftSection">
+                        <div class="mainDetails" >
+                            <div class="Objective" >
+                                <p style="font-weight: bold;font-style: italic;font-size: 18px;">OBJECTIVES:</p>
+                                <p style="font-size: 14px;">' . $final_summary . '</p>
+                            </div>
+                            <div class="Experience">
+                                <p id="PE" style="font-weight: bold;font-style: italic;font-size: 16px;">EXPERIENCE:</p>
+                                <p>' . $ex2 . '</p>
+                            </div>
                         </div>
-                        <div class="Experience">
-                            <p id="PE" style="font-weight: bold;">EXPERIENCE:</p>
-                            
+                    </div>
+                </div>
+                <div class="col-xs-3 " >
+                    <div class="infoRightSection">
+                        <div class="extraDetails">
+                            <div class="email">
+                                <p style="font-weight: bold">Email:</p>
+                                <p>' . $the_big_array[10][2] . '</p>
+                            </div>
+                            <div class="phone">
+                                <p style="font-weight: bold">Phone:</p>
+                                <p>' . $the_big_array[10][3] . '</p>
+                            </div>
+                            <div class="linkL">
+                                <p style="font-weight: bold">Link:</p>
+                                ' . $the_big_array[10][1] . '
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-xs-3 ">
-                <div class="infoRightSection">
-                    <div class="extraDetails">
-                        <div class="email">
-                            <p style="font-weight: bold">Email:</p>
-                            <p>' . $the_big_array[10][2] . '</p>
-                        </div>
-                        <div class="phone">
-                            <p style="font-weight: bold">Phone:</p>
-                            <p>' . $the_big_array[10][3] . '</p>
-                        </div>
-                        <div class="linkL">
-                            <p style="font-weight: bold">Link:</p>
-                            ' . $the_big_array[10][1] . '
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
-</body>';
+    </body>
+</html>';
+
+//echo $html;
+//die;
 
 $stylesheet = file_get_contents('lib/bootstrap.min.css');
 $stylesheet2 = file_get_contents('lib/main.css');
